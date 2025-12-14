169ad661537d72eed247cef4d7424309c31f3d24
Amit singh <notgetin18@gmail.com>
2025-12-15 01:03:04 +0530

    scroll to

--- src/components/home/navbar.tsx
+++ src/components/home/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Download, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { useState, useMemo } from "react";
@@ -9,6 +8,7 @@ import { handleDownloadResume } from "@/utility";
 import Image from "next/image";
 import amitImage from "../../../public/favicons/web-app-manifest-192x192.png";
 import { usePathname } from "next/navigation";
+import { Download, Menu, X, Home, User, Mail, Settings, BookOpen, ArrowBigLeft } from "lucide-react";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -20,11 +20,36 @@ export default function Navbar() {
 
   const navLinks = useMemo(
     () => [
-      { href: "/", label: "Home" },
-      { href: "/about", label: "About" },
-      { href: "/contact", label: "Contact" },
-      { href: "/services", label: "Services" },
-      { href: "/blog", label: "Blog" },
+      {
+        href: "/",
+        label: "Home",
+        icon: Home,
+        description: "Discover my latest projects and journey as a MERN developer."
+      },
+      {
+        href: "/about",
+        label: "About",
+        icon: User,
+        description: "Learn about my skills, experience, and passion for full-stack engineering."
+      },
+      {
+        href: "/contact",
+        label: "Contact",
+        icon: Mail,
+        description: "Get in touch for collaborations, consultations, or quick chats."
+      },
+      {
+        href: "/services",
+        label: "Services",
+        icon: Settings,
+        description: "Explore end-to-end development solutions from MVP to scale."
+      },
+      {
+        href: "/blog",
+        label: "Blog",
+        icon: BookOpen,
+        description: "Read insights on MERN, performance tips, and real-world dev stories."
+      },
     ],
     []
   );
@@ -39,20 +64,22 @@ export default function Navbar() {
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
-      className="w-full max-w-7xl fixed top-3 -translate-x-1/2 bg-[#07162b]/50 backdrop-blur-lg z-20 border border-white/15 rounded-xl sm:rounded-3xl shadow-lg shadow-black/20"
+      className={`w-full max-w-7xl fixed top-3 -translate-x-1/2 bg-[#07162b]/50 backdrop-blur-lg z-20 border-[0.5px] sm:border border-[#06b6d4] sm:border-white/15 rounded-xl sm:rounded-3xl shadow-[#06b6d4] sm:shadow-black/20 ${isMenuOpen ? "shadow-sm" : ""}`}
     >
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-2">
-          <motion.div whileHover={{ scale: 1.05 }}>
-            <Image
-              src={amitImage}
-              width={192}
-              height={192}
-              alt="amit image"
-              className="h-16 w-16 rounded-full border-2 border-white/50 shadow-md shadow-white/80"
-              priority
-            />
-          </motion.div>
+          <Link href="/">
+            <motion.div whileHover={{ scale: 1.05 }}>
+              <Image
+                src={amitImage}
+                width={192}
+                height={192}
+                alt="amit image"
+                className="h-16 w-16 rounded-full border-2 border-white/80 shadow-md shadow-white/50"
+                priority
+              />
+            </motion.div>
+          </Link>
 
           {/* Desktop Links */}
           <div className="hidden md:flex space-x-8 border border-white/20 rounded-full px-8 py-3 shadow-sm shadow-white/30 backdrop-blur-lg">
@@ -62,11 +89,10 @@ export default function Navbar() {
                 <Link
                   key={link.href}
                   href={link.href}
-                  className={`font-semibold transition-colors duration-300 ${
-                    isActive
-                      ? "bg-clip-text text-transparent bg-gradient-to-r from-[#8ef3c1] via-[#3ed6ac] to-[#06b6d4]"
-                      : "text-slate-300 hover:text-[#3ed6ac]"
-                  }`}
+                  className={`font-semibold transition-colors duration-300 ${isActive
+                    ? "bg-clip-text text-transparent bg-gradient-to-r from-[#8ef3c1] via-[#3ed6ac] to-[#06b6d4]"
+                    : "text-slate-300 hover:text-[#3ed6ac]"
+                    }`}
                   aria-label={`Go to ${link.label} page`}
                 >
                   {link.label}
@@ -79,7 +105,7 @@ export default function Navbar() {
           <div className="md:hidden text-white">
             <button
               onClick={toggleMenu}
-              className="text-white border border-white/30 shadow-lg shadow-white/20 transition-colors duration-300 hover:text-[#3ed6ac] inset-0 bg-gradient-to-tr from-black/30 via-transparent to-black/10 mix-blend-overlay rounded-md p-1 shadow-lg shadow-black/50"
+              className="text-white border border-white/80 shadow-white/40 transition-colors duration-300 hover:text-[#3ed6ac] inset-0 bg-gradient-to-tr from-black/30 via-transparent to-black/10 mix-blend-overlay rounded-md p-1 shadow-lg"
               aria-label={isMenuOpen ? "Close menu" : "Open menu"}
             >
               {isMenuOpen ? (
@@ -125,34 +151,45 @@ export default function Navbar() {
           initial="closed"
           animate={isMenuOpen ? "open" : "closed"}
           className="md:hidden overflow-hidden"
-          // transition-colors duration-300 bg-[#07162b]/50 backdrop-blur-lg border border-white/15 rounded-3xl shadow-lg shadow-black/20
+        // transition-colors duration-300 bg-[#07162b]/50 backdrop-blur-lg border border-white/15 rounded-3xl shadow-lg shadow-black/20
         >
-          <div className="flex flex-col space-y-4 pb-4">
+          <div className="px-4 py-4 my-3 mb-4 bg-white/15 border-b border-white/20 shadow-black shadow-md rounded-sm">
+            <h2 className="text-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#8ef3c1] via-[#3ed6ac] to-[#06b6d4] mb-0.5">Quick Navigation</h2>
+            <p className="text-sm text-slate-300">Choose the way that works for you.</p>
+          </div>
+
+          <div className="flex flex-col space-y-4 pb-3 overflow-y-auto h-full border border-white/20 rounded-xl p-5 mb-5">
             {navLinks.map((link) => {
               const isActive = pathname === link.href;
               return (
                 <Link
                   key={`mobile-${link.href}`}
                   href={link.href}
-                  className={`text-center font-semibold transition-colors duration-300 py-2 ${
-                    isActive
-                      ? "bg-clip-text text-transparent bg-gradient-to-r from-[#8ef3c1] via-[#3ed6ac] to-[#06b6d4]"
-                      : "text-slate-200 hover:text-[#3ed6ac]"
-                  }`}
+                  className={`flex items-start gap-3 pl-4 rounded-lg transition-all duration-300 ${isActive
+                    ? "bg-gradient-to-r from-[#8ef3c1]/20 to-[#06b6d4]/20 border border-[#06b6d4]/90 text-white shadow-md"
+                    : "text-slate-300 hover:text-white hover:bg-white/10 bg-white/5 border border-white/40 hover:border-white/20 transition-colors duration-300"
+                    }`}
                   onClick={() => setIsMenuOpen(false)}
                   aria-label={`Go to ${link.label} page`}
                 >
-                  {link.label}
+                  <link.icon className={`w-8 h-8 flex-shrink-0 top-1/2 translate-y-1/2 ${isActive ? "text-[#06b6d4]" : "text-slate-400 bg-white/10 rounded  px-1"}`} />
+                  <div className="flex-1">
+                    <span className="font-semibold block py-1">{link.label}</span>
+                    <p className="text-[10px] leading-relaxed text-slate-200 mb-1.5">{link.description}</p>
+                    {/* {isActive && <span className="text-xs text-[#06b6d4] mt-1 block">Active</span>} */}
+                  </div>
+                  <span className={`text-[#06b6d4] text-sm font-medium arrow ${isActive ? "opacity-100" : "opacity-0"}`}><ArrowBigLeft className="m-0.5" /></span>
                 </Link>
               );
             })}
+
             <Button
               onClick={() => {
                 handleDownloadResume("pdf");
                 setIsMenuOpen(false);
               }}
               style={{
-                fontWeight: 600,
+                fontWeight: 900,
                 borderRadius: "20px",
                 border: "1px solid #F9DDB5",
                 background:
@@ -165,8 +202,8 @@ export default function Navbar() {
               aria-label="Download Amit Kumar's resume in PDF format"
             >
               <Download
-                className="w-4 h-4 mr-1.5"
-                style={{ fontWeight: 600 }}
+                className="w-4 h-4 mr-1"
+                style={{ fontWeight: 900, fontSize: "20px", color: "#fffem" }}
               />
               Resume
             </Button>
@@ -271,7 +308,7 @@ export default function Navbar() {
 //             <div className="md:hidden text-white">
 //               <button
 //                 onClick={toggleMenu}
-//                 className="text-white border border-white/30 shadow-lg shadow-white/20 transition-colors duration-300 hover:text-[#3ed6ac] inset-0 bg-gradient-to-tr from-black/30 via-transparent to-black/10 mix-blend-overlay rounded-md p-2 shadow-lg shadow-black/50"
+//                 className="text-white border border-white/30 transition-colors duration-300 hover:text-[#3ed6ac] inset-0 bg-gradient-to-tr from-black/30 via-transparent to-black/10 mix-blend-overlay rounded-md p-2 shadow-lg shadow-black/50"
 //                 aria-label={isMenuOpen ? "Close menu" : "Open menu"}
 //               >
 //                 {isMenuOpen ? (
